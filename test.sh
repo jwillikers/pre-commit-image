f89430c3925566fd7716d2714bcea8f0e3855180
Jordan Williams <jordan@jwillikers.com>
2021-06-25 10:27:11 -0500

    Fix volume directory path

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -o errexit
 # Share storage.
 # -v ~/.local/share/containers/storage:~/.local/share/containers/storage/shared:ro
 
-podman run --rm --userns keep-id --user podman:podman --device /dev/fuse --security-opt label=disable --volume pre-commit-image:~/.local/share/containers/storage --name test-container localhost/pre-commit podman run registry.fedoraproject.org/fedora-minimal:latest echo "Hello World!"
+podman run --rm --userns keep-id --device /dev/fuse --security-opt label=disable --volume pre-commit-image:/home/podman/.local/share/containers/storage --name test-container localhost/pre-commit podman run registry.fedoraproject.org/fedora-minimal:latest echo "Hello World!"
