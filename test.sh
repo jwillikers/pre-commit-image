dc85fa7890738965d8c38f6895e02a7ca7706d6e
Jordan Williams <jordan@jwillikers.com>
2021-06-25 17:11:08 +0000

    Create actual test case and fix image URL's

--- test.sh
+++ test.sh
@@ -4,4 +4,88 @@ set -o errexit
 # Share storage.
 # -v ~/.local/share/containers/storage:~/.local/share/containers/storage/shared:ro
 
-podman run --rm --userns keep-id --device /dev/fuse --security-opt label=disable --volume pre-commit-image:/home/podman/.local/share/containers/storage --name test-container localhost/pre-commit podman run registry.fedoraproject.org/fedora-minimal:latest echo "Hello World!"
+# Create a temporary directory
+test_dir=$(mktemp -d -t test-pre-commit-image.XXXXXXXXXX)
+
+# Configure a Git repository to use as a test case
+git -C "$test_dir" init
+git -C "$test_dir" config user.email "tester@testing.test"
+git -C "$test_dir" config user.name "Tester Testington"
+git -C "$test_dir" config commit.gpgSign false
+
+# Add and stage a test file and a pre-commit configuration file
+
+echo '#!/usr/bin/env bash
+set -o errexit
+
+echo "Hello World!"
+
+' >"$test_dir/test.sh"
+git -C "$test_dir" add test.sh
+
+echo '
+repos:
+  - repo: https://github.com/pre-commit/pre-commit-hooks
+    rev: v4.0.1
+    hooks:
+      - id: check-byte-order-marker
+      - id: check-executables-have-shebangs
+      - id: check-json
+      - id: check-merge-conflict
+      - id: check-yaml
+      - id: end-of-file-fixer
+      - id: mixed-line-ending
+      - id: trailing-whitespace
+  - repo: local
+    hooks:
+      - id: shellcheck
+        name: shellcheck
+        language: system
+        entry: podman run --rm -v /home/podman/mnt:/mnt:Z docker.io/koalaman/shellcheck:latest
+        args: [--color=always]
+        types: [shell]
+  - repo: local
+    hooks:
+      - id: shfmt
+        name: shfmt
+        language: system
+        entry: podman run --rm -v /home/podman/mnt:/mnt:Z -w /mnt docker.io/mvdan/shfmt:latest
+        args: [-w]
+        types: [shell]
+
+' >"$test_dir/.pre-commit-config.yaml"
+git -C "$test_dir" add .pre-commit-config.yaml
+
+# Test pre-commit install
+podman run \
+	--rm \
+	--userns keep-id \
+	--device /dev/fuse \
+	--security-opt label=disable \
+	-v "$test_dir":/home/podman/mnt:Z \
+	-v ~/.config/git:/home/podman/.config/git:ro,z \
+	-v pre-commit:/home/podman/.local/share/containers/storage \
+	--name pre-commit \
+	localhost/pre-commit \
+	pre-commit install
+
+# Test that running the pre-commit hooks works
+podman run \
+	--rm \
+	--userns keep-id \
+	--device /dev/fuse \
+	--security-opt label=disable \
+	-v "$test_dir":/home/podman/mnt:Z \
+	-v ~/.config/git:/home/podman/.config/git:ro,z \
+	-v "$HOME/.gnupg":/home/podman/.gnupg:ro,z \
+	-v "$HOME/.ssh":/home/podman/.ssh:ro,z \
+	-v "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent":/home/podman/.gnupg/S.gpg-agent \
+	-v "$SSH_AUTH_SOCK":"$SSH_AUTH_SOCK" \
+	-e SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
+	-v pre-commit:/home/podman/.local/share/containers/storage \
+	--name pre-commit \
+	localhost/pre-commit \
+	git commit -m "Test commit"
+
+# Cleanup
+rm -rf "$test_dir"
